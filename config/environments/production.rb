3a128821deffbed496ae0d49467ddac5dd1744f7
yasir saeed <yasirsaeed@ubuntu.ubuntu-domain>
2014-12-08 14:40:51 -0800

    assets changes added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-
+  config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
