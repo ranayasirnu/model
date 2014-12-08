045bb590b5a7d9e5b3b3560d803ea92213d3af63
yasir saeed <yasirsaeed@ubuntu.ubuntu-domain>
2014-12-08 15:20:32 -0800

    admin panel user extra fields removed

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -17,6 +17,25 @@ ActiveAdmin.register User do
     actions
   end
   
+  form do |f|
+    f.inputs "User information" do
+      f.input :user_id
+      f.input :location
+      f.input :status
+    end
+    f.actions
+  end
+  
+   show do |cause|
+    attributes_table do
+      row :id
+      row :user_id
+      row :location
+      row :status
+    end
+    active_admin_comments
+  end
+   
   
   # permit_params :list, :of, :attributes, :on, :model
   #
