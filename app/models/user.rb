6d2a697f19f1afbf0a8af5f488fb50fbbc7c7b67
yasir saeed <yasirsaeed@ubuntu.ubuntu-domain>
2014-12-08 14:13:32 -0800

    user login status changes added

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,15 @@ class User < ActiveRecord::Base
          
          
   def self.find_for_facebook_mobile_oauth(user_json)
-    puts "==========dddddd====: #{user_json['user_id']}"
-    puts "==========dddddd====: #{user_json['location']}"
-    puts "==========dddddd====: #{user_json['status']}"
     
     user = User.where(:user_id => user_json['user_id']).first
     if user.present?
       user.update_attributes(
-                      location: user_json['location'],
-                      status: user_json['status'],
+                      location: user_json['location']
                     ) 
     else
       user = User.create(
                         location: user_json['location'],
-                        status: user_json['status'],
                         user_id:user_json['user_id']
                       )
     end
